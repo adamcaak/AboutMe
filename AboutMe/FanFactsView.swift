aec5b2665b9e4c4c13744fb26c099abf48125936
adamcaak <ladamczak1997@gmail.com>
2025-06-26 00:20:19 +0200

    Create storyview, homeview, favoritesview, fanfactsview

---
+++ AboutMe/FanFactsView.swift
@@ -0,0 +1,18 @@
+//
+//  FanFactsView.swift
+//  AboutMe
+//
+//  Created by Łukasz Adamczak on 26/06/2025.
+//
+
+import SwiftUI
+
+struct FanFactsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FanFactsView()
+}
---
+++ AboutMe/StoryView.swift
@@ -0,0 +1,18 @@
+//
+//  StoryView.swift
+//  AboutMe
+//
+//  Created by Łukasz Adamczak on 26/06/2025.
+//
+
+import SwiftUI
+
+struct StoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoryView()
+}
