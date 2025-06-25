4c806a6c7cb17785a587de11dcf242e98b601564
adamcaak <ladamczak1997@gmail.com>
2025-06-26 00:14:10 +0200

    Create data, add image, create file home view

---
+++ AboutMe/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  AboutMe
+//
+//  Created by Łukasz Adamczak on 26/06/2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
