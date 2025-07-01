3eb94d6daebfb24dc8471e3fa928651ad79795e9
adamcaak <ladamczak1997@gmail.com>
2025-07-01 21:10:44 +0200

    Create MyHistory

---
+++ AboutMe/MyHistory.swift
@@ -0,0 +1,18 @@
+//
+//  MyHistory.swift
+//  AboutMe
+//
+//  Created by Łukasz Adamczak on 01/07/2025.
+//
+
+import SwiftUI
+
+struct MyHistory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyHistory()
+}
