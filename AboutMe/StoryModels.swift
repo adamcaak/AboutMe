353efe43ebee24185fba1c494ea553457e44780a
adamcaak <ladamczak1997@gmail.com>
2025-07-01 21:26:43 +0200

    Create Model Story

---
+++ AboutMe/StoryModels.swift
@@ -0,0 +1,33 @@
+//
+//  StoryModels.swift
+//  AboutMe
+//
+//  Created by Łukasz Adamczak on 01/07/2025.
+//
+
+import Foundation
+
+struct Story {
+    
+    let pages: [StoryPage]
+    
+    subscript(_ pageIndex: Int) -> StoryPage {
+        return pages(pageIndex)
+    }
+}
+
+struct StoryPage {
+    let text: String
+    
+    let choice: [Choice]
+    
+    init(_ text: String, choice: [Choice]) {
+        self.text = text
+        self.choice = choice
+    }
+}
+
+struct Choice {
+    let text: String
+    let destination: Int
+}
